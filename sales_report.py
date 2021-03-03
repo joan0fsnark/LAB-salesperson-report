a1f67a829ac633635a81b88a887af2a06a1b5604
Alena <alenad@gmail.com>
2021-03-03 03:42:22 +0000

    Made comments about code.

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,34 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-
+# creates empty lists
 salespeople = []
 melons_sold = []
 
+# opens the sales report file, 
+# breaks it down line-by-line, 
+# returns a copy of the string with trailing characters removed
+# splits the line into elements at the pipe character and assigns them
+# to 'entries' variable
 f = open('sales-report.txt')
 for line in f:
     line = line.rstrip()
     entries = line.split('|')
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+    salesperson = entries[0] #assigns first element of entries to salesperson variable
+    melons = int(entries[2]) #assigns third element of entries to melons variable
 
+    # if the value of salesperson is in the salespeople list,
+    #    then the person at specified index of salespeople list is saved to variable 'position'
     if salesperson in salespeople:
         position = salespeople.index(salesperson)
 
+        # increases count of melons_sold at 'position' by amount of 'melons'
         melons_sold[position] += melons
-    else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    else: # otherwise:
+        salespeople.append(salesperson) # add salesperson to 'salespeople' list
+        melons_sold.append(melons) # add 'melons' to melons_sold list
 
-
+# for each person in salespeople
 for i in range(len(salespeople)):
+    # print <person> sold <amount of> melons'
     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
